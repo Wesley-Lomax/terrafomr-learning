70d04d0ab51cf44c7afd9f17961d3032f280a2c1
WesleyLomax <wesley@principlelogic.co.uk>
2021-09-16 17:13:33 +0100

    Updated script and azurerm version

--- Azure/DeployWebApp/AppService.tf
+++ Azure/DeployWebApp/AppService.tf
@@ -4,25 +4,47 @@ As in previous labs, be sure to copy in the lab generated resource group name.
 https://app.linuxacademy.com/hands-on-labs/343bd2cd-152c-4d93-8638-141efad836dd
   */
 
-provider "azurerm" {
-  version = 1.38
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {  
+  features {}
+  
+  /* 
+  subscription_id   = "<azure_subscription_id>"
+  tenant_id         = "<azure_subscription_tenant_id>"
+  client_id         = "<service_principal_appid>"
+  client_secret     = "<service_principal_password>" 
+  */
+}
+
+resource "azurerm_resource_group" "test_rg" {
+  name     = "rg-test-resources"
+  location = "UK South"
 }
 
 resource "azurerm_app_service_plan" "svcplan" {
   name                = "wesley-testing-terraform-hp"
-  location            = "eastus"
-  resource_group_name = "191-0aa601a8-deploy-a-web-application-with-terrafo"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
 
   sku {
     tier = "Standard"
     size = "S1"
+    capacity = "2"
   }
 }
 
 resource "azurerm_app_service" "appsvc" {
   name                = "wesley-testing-terraform-app"
-  location            = "eastus"
-  resource_group_name = "191-0aa601a8-deploy-a-web-application-with-terrafo"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
   app_service_plan_id = azurerm_app_service_plan.svcplan.id
 
 
